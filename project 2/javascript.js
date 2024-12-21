9a1667752aed55a7f277ff1f62a91e2f1c15c1fb
chetan-babu125 <chetanbabu125@gmail.com>
2024-12-21 09:29:30 +0530

    Add files via upload

---
+++ project 2/javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("visitorForm").addEventListener("submit", function(event) 
+{
+    event.preventDefault();
+    alert("Registration submitted successfully! We look forward to seeing you at Tech Innovators Conference 2024! Thank You");
+}
+);
